d04364c1298ca3018d74770f9aec35179032b59e
12moi <moi.shadrack@student.moringaschool.com>
2023-01-22 20:37:13 +0300

    add changes

--- src/Search.js
+++ src/Search.js
@@ -24,6 +24,7 @@ const Search = (props) => {
           value={searchValue}
           onChange={handleSearchInputChanges}
           type="text"
+          placeholder="Search your favorite movie here.."
         />
         <input onClick={callSearchFunction} type="submit" value="SEARCH" />
       </form>
